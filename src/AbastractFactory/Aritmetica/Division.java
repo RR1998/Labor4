d9473712c5b7bcbbd8d927fdf4f78c4ba5bde5e9
LN710Q <LN710Q@DEILABO1-PC27>
2018-05-23 12:33:55 -0600

    comiteado papu

---
+++ src/AbastractFactory/Aritmetica/Division.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AbastractFactory.Aritmetica;
+
+/**
+ *
+ * @author LN710Q
+ */
+public class Division implements Aritmetica{
+    @Override
+    public int resultado (int val1,int val2){
+        return val1/val2;
+    }
+}
