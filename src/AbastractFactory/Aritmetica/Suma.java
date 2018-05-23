409727323f50f348db04c49e04f64459b99812b4
LN710Q <LN710Q@DEILABO1-PC27>
2018-05-23 12:25:04 -0600

    comiteado papu

---
+++ src/AbastractFactory/Aritmetica/Suma.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AbastractFactory.Aritmetica;
+
+/**
+ *
+ * @author LN710Q
+ */
+public class Suma implements Aritmetica {
+    @Override
+    public int resultado(int val1, int val2){
+        
+        
+    }
+}
